e739fb9992167b9523a68c7c2f7fe2439379c13d
jeo19 <terrakkk@outlook.com>
2018-08-22 01:55:38 +0630

    git commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import PhoneForm from "./MyName";
+import PhoneInfoList from "./PhoneInfoList";
+
+class App extends Component {
+  id = 2;
+  state = {
+    information: [
+      {
+        id: 0,
+        name: "김민준",
+        phone: "010-0000-0000"
+      },
+      {
+        id: 1,
+        name: "홍길동",
+        phone: "010-0000-0001"
+      }
+    ]
+  };
+  handleCreate = data => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    });
+  };
+  render() {
+    return (
+      <div>
+        <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList data={this.state.information} />
+      </div>
+    );
+  }
+}
+
+export default App;
